4e47265005d7024d836acb535959f0d089fabcaa
DavidOsezua <102161226+DavidOsezua@users.noreply.github.com>
2024-12-10 02:24:39 +0100

    My first commit after finishing Introduction and half of chapter one

---
+++ Eloquent-javascript/Chapter-one/takeaways.md
@@ -0,0 +1,24 @@
+# Key points
+
+- Any Information can be reduced into zeros and ones therefore can be represented as bits
+- Calculations on whole numbers(integers) are guaranted to be precised but fractions are generally not so. It is advisable to treat fractions as approximations rather than precise values.
+- Values in Javascripts can be combined and transformed by the use of operators. These operators are of four types.
+
+* Unary Operator(typeOf,!)
+* Binary Operator(+, -,\*,/)
+* tenary Operator(?:)
+* Logical Operator(&&,||,??)
+
+-Strings, which are texts in javascript also needs to be modeled by bits in order for them to exist inside the computer's memory.The way this is done is through the unicode standard of the text.unicode standard assigns numbers to every character in the world
+
+-When comparing strings in javascript, javascript compares the characters of the text from left to right using their unicode codes one by one
+
+-The only value in javascript that is not equal to itself is NaN
+
+- Logical operators are operators used to perform operations on boolean value
+
+-Type Coercion is concept in javascript where by javascript gives a type to a value that it thinks it needs when two values to be transformed are of different types.
+
+-To prevent unneccessary type coercion, the use of three character comparison is your way to go.(=== or !==)
+
+-
---
+++ Eloquent-javascript/Introduction/myIntro.js
@@ -0,0 +1,74 @@
+//This code was written from memory after studing the book's code "intro.js"
+
+//The code below is a function that returns an arr of a range of numbers
+function range(start, end, step = 1) {
+  const arr = [];
+  if (start > end) {
+    for (let i = start; i >= end; i -= step) {
+      arr.push(i); // 10,9,
+    }
+  } else {
+    for (let i = start; i <= end; i += step) {
+      arr.push(i);
+    }
+  }
+
+  return arr;
+}
+
+//This function returns the sum of an array of numbers
+function sum(arr) {
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+
+  return total;
+}
+
+//puting both functions in one. Just experimenting
+function sumOfRange(start, end, step = 1) {
+  const arr = [];
+  let total = 0;
+  if (start > end) {
+    for (let i = start; i >= end; i -= step) {
+      arr.push(i);
+    }
+  } else {
+    for (let i = start; i <= end; i += step) {
+      arr.push(i);
+    }
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+
+  return total;
+}
+
+// const arr = range(10, 1);
+// const sumOfNumbers = sum(arr);
+
+// console.log(sum(range(3, 15, 2)));
+
+//A function that gets the factorial of a number
+function factorial(number) {
+  let factorial = number;
+  let arr = [];
+  for (let i = number - 1; i >= 1; i--) {
+    factorial *= i;
+    arr.push(i);
+  }
+
+  return { factorial, arr };
+}
+
+console.log(factorial(5));
+console.log(factorial(8));
+console.log(factorial(9));
+console.log(factorial(10));
+console.log(factorial(-10));
+
+//5factorial === 5*4*3*2*1
+//6factorial === 6*5*4*3*2*1
