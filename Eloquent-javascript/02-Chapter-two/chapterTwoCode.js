561c08457b1621b6dc9e27fa6aa06ed4e74bc2fd
DavidOsezua <102161226+DavidOsezua@users.noreply.github.com>
2025-01-13 11:17:12 +0100

    continued studying chapter two of elequent javascript

---
+++ Eloquent-javascript/02-Chapter-two/chapterTwoCode.js
@@ -0,0 +1,36 @@
+//CONTROL FLOW
+//Conditional Execution
+// let theNumber = Number(prompt("Enter a number:"));
+// if (!Number.isNaN(theNumber)) {
+//   console.log(`the square of the number is :${theNumber * theNumber}`);
+// }
+
+// let num = Number(prompt("Pick a number:"));
+
+// if (num < 10) {
+//   console.log("small");
+// } else if (num < 100) {
+//   console.log("medium");
+// } else {
+//   console.log("large");
+// }
+
+// A Loop that multiplies 2 10 times
+// let num = 1;
+// let count = 0;
+
+// while (count < 10) {
+//   num = num * 2;
+//   count = count + 1;
+// }
+
+// console.log(num);
+
+
+//Using The Break Key Word.
+// for (let current = 20; (current = current + 1); ) {
+//   if (current % 7 === 0) {
+//     console.log(current);
+//     break;
+//   }
+// }
