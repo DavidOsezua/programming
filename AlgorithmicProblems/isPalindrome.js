cfb9caa93ba5c807ec45487584cdf28eb436281b
DavidOsezua <102161226+DavidOsezua@users.noreply.github.com>
2025-05-27 14:06:38 +0100

    ...

--- AlgorithmicProblems/isPalindrome.js
+++ AlgorithmicProblems/isPalindrome.js
@@ -1,22 +1,42 @@
+// function isPalindrome(s) {
+//   let forwardResult = [];
+//   let backwardResult = [];
+
+//   let ptr1 = 0;
+//   let ptr2 = s.length - 1;
+
+//   for (ptr1, ptr2; ptr1 <= s.length - 1 || ptr2 >= 0; ptr1++, ptr2--) {
+//     forwardResult.push(s[ptr1]);
+//     backwardResult.push(s[ptr2]);
+//   }
+
+//   if (forwardResult.join("") === backwardResult.join("")) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+//My algorithm is bad even though it uses a time complexity of O(n), it uses a Space Complexity of  O(n)
+
+
+
+// CORRECT
 function isPalindrome(s) {
-  let forwardResult = [];
-  let backwardResult = [];
+  let left = 0,
+    right = s.length - 1;
 
-  let ptr1 = 0;
-  let ptr2 = s.length - 1;
+  while (left < right) {
+    if (s[left] != s[right]) {
+      return false;
+    }
 
-  for (ptr1, ptr2; ptr1 <= s.length - 1 || ptr2 >= 0; ptr1++, ptr2--) {
-    forwardResult.push(s[ptr1]);
-    backwardResult.push(s[ptr2]);
+    right--;
+    left++;
   }
 
-  if (forwardResult.join("") === backwardResult.join("")) {
-    return true;
-  } else {
-    return false;
-  }
+  return true;
 }
-
 function main() {
   const tests = ["kayak", "hello", "RACEACAR", "A", " ABCDABCD"];
 
